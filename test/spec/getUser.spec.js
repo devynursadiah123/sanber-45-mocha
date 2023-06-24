e615c4ef2c5cd66e375434a6c2a6b536bca770bb
devynursadiah123 <devynursadiah@gmail.com>
2023-06-24 16:51:04 +0700

    add super test

---
+++ test/spec/getUser.spec.js
@@ -0,0 +1,33 @@
+const request = require("supertest")("https://reqres.in/api"); //baseurl dlm string
+const expect = require("chai").expect;
+
+async function getUserPages(page){
+  const response = await request.get(`/users?page=${page}`);
+  return response
+}
+
+//test suite
+describe("GET /users", function(){
+   //test case
+    it("returns users page 2", async function(){
+        const query1 = "2"
+        const response = await getUserPages(query1);
+        console.log(await response.body)
+        
+
+        expect(await response.status).to.eql(200);
+        expect(await response.body.data.length).to.eql(6);
+    })
+
+    it("returns users page 3", async function(){
+      const query1 = "3"
+      const response = await getUserPages(query1);
+      console.log(await response.body)
+      
+
+      expect(await response.status).to.eql(200);
+      expect(await response.body.data.length).to.eql(0);
+  })
+})
+
+exports.getUserPages = { getUserPages }
